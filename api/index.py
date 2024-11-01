50c190e7f90a48d767d3d27497b35d0d4d5d9bff
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-11-01 20:57:02 +0100

    fix error deploy 2

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
-import sys
+from flask import Flask, request, render_template, send_file
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Aggiungi la directory corrente al path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+from app import app
 
-from app import app as application
+# Necessario per Vercel
+if __name__ == '__main__':
+    app.run()
